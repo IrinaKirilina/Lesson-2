6c3ed48dbcb480579ff72366b4107d3b49f4a651
IrinaKirilina <boga.irina@mail.ru>
2021-12-13 20:36:11 -0800

    Lesson 2

--- five.rb
+++ five.rb
@@ -13,11 +13,7 @@ day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day_in_month[1] = 29 if year % 4 == 0
 
-day_in_month[0...month-1].each do |m|
-	sum += m
-	end
-
-sum = sum + day
+sum = day_in_month.take(month-1).sum + day
 
 	puts "Serial number: #{sum}"
 
--- four.rb
+++ four.rb
@@ -3,7 +3,8 @@ vowels = {}
 alphabet  = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,s,t,u,v,w,x,y,z]
 vowels_letter= [a,e,i,o,u,y]
 
-vowels.each do |vowel| alphabet.index([vowel]) + 1
+vowels.each do |vowel| 
+	alphabet.index([vowel]) + 1
 
 end
 
